6d2c37f5f99820d3478ca2f62f5ff90f5a649d54
Donika <donika.lazarova11@gmail.com>
2026-05-24 10:58:46 +0300

    Първоначално качване на проекта

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(express.static('./'));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('./')); // Това обслужва файловете ти (index.html, script.js)
+
+// Път, който зарежда сайта
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-app.use(cors());
-app.use(express.json());
 
 // Масиви - твоята временна база данни
 let users = [
-  { id: 1, name: "Доника", email: "donika@test.com", points: 0, level: 1 } // Добавяме теб като тестов потребител №1
+  { id: 1, name: "Доника", email: "donika@test.com", points: 0, level: 1 }
 ];
 let tasks = [];
-let questions = [];
 
 // Пътища (Routes)
 app.get('/users', (req, res) => res.json(users));
-
-app.post('/register', (req, res) => {
-  const { name, email } = req.body;
-  const newUser = { id: users.length + 1, name, email, points: 0, level: 1 };
-  users.push(newUser);
-  res.status(201).json(newUser);
-});
 
 app.post('/tasks', (req, res) => {
   const { title, points, userId } = req.body;
@@ -32,7 +27,7 @@ app.post('/tasks', (req, res) => {
 
   const newTask = { id: tasks.length + 1, title, points, userId, completed: true };
   tasks.push(newTask);
-  user.points += points; // Увеличаваме точките на потребителя в масива
+  user.points += points; 
   res.status(201).json(newTask);
 });
 
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-async function addPoints() {
-    console.log("Бутонът е натиснат!"); 
+ console.log("Бутонът е натиснат!"); 
     
     // Пращаме заявка към сървъра (localhost:3000)
-    await fetch('http://localhost:3000/tasks', {
+    await fetch('https://my-app-nwwp.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
@@ -11,15 +10,7 @@ async function addPoints() {
             userId: 1 // Това си ти (Доника)
         })
     });
-
-    updateUI(); // Обновяваме числата на екрана
-}
-
-async function updateUI() {
-    const response = await fetch('http://localhost:3000/market-value/1');
+    const response = await fetch('https://my-app-nwwp.onrender.com/market-value/1');
     const data = await response.json();
-    
-    // Сменяме текста в HTML-а
-    document.getElementById('points-display').innerText = data.points;
-    document.getElementById('value-display').innerText = data.marketValue;
+
 }
